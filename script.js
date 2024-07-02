5da803fdc9d1f590247b2338e8f17b828fc937f6
LakkireddySujitha <lakkireddy.sujitha1234@gmail.com>
2024-07-02 07:09:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function toCase(text) {
   // write your code here
+	if (text.length === 0) {
+    return '-';
+  }
+const lowerCase = text.toLowerCase();
+const upperCase = text.toUpperCase();
+ return `${lowerCase}-${upperCase}`;
 }
 
 // DO not change the code below
